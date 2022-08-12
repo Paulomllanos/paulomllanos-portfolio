e689fb343059246e11402ce6fd615b55ceff7696
Paulo Maldonado <paulomllanos@gmail.com>
2022-08-11 22:06:02 -0400

    fix path

--- src/components/MailChimpForm.js
+++ src/components/MailChimpForm.js
@@ -1,9 +1,8 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 import { Newsletter } from "./Newsletter";
 
-if(process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-  }
+// require('dotenv').config();
+
 
 export const MailchimpForm = () => {
   const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
